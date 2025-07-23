1bc0fe442c1bd82e8ad9fe0792a7bdafb337e42b
Dikshan kc <146184803+Dikshan23@users.noreply.github.com>
2025-07-23 19:07:07 +0545

    Add project files.

---
+++ StudentManagement/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Models;
+using System.Collections.Generic;
+using System.Linq;
+namespace StudentManagement.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static List<Student> students = new List<Student>
+        {
+            new Student { Id =1 , Name = "Dikshan KC", Age = 22 },
+            new Student { Id= 2, Name ="Sabina KC", Age = 21 },
+            new Student { Id = 3, Name ="Aayam KC", Age = 9}
+        };
+
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+
+        public IActionResult Create()
+        {
+                       return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                student.Id = students.Any() ? students.Max(s => s.Id) + 1: 1; // Assign a new ID
+                students.Add(student);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+    }
+}
