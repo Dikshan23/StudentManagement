7a2a81852dfe2e2ac6f1eee9969a6f617f2cbc4a
Dikshan kc <146184803+Dikshan23@users.noreply.github.com>
2025-07-31 17:47:57 +0545

    upadte

--- StudentManagement/Controllers/StudentController.cs
+++ StudentManagement/Controllers/StudentController.cs
@@ -8,15 +8,25 @@ namespace StudentManagement.Controllers
     {
         private static List<Student> students = new List<Student>
         {
-            new Student { Id =1 , Name = "Dikshan KC", Age = 22 },
-            new Student { Id= 2, Name ="Sabina KC", Age = 21 },
-            new Student { Id = 3, Name ="Aayam KC", Age = 9}
+            new Student { Id =1 , Name = "Dikshan KC",Email ="dixan294@gmail.com", Age = 22 },
+            new Student { Id= 2, Name ="Sabina KC",Email ="Sabina23@gmail.com", Age = 21 },
+            new Student { Id = 3, Name ="Aayam KC",Email = "aayam13@gmail.com",Age = 9}
         };
 
         public IActionResult Index()
         {
             return View(students);
         }
+
+        public IActionResult Confirmation(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+            if (student == null)
+                return NotFound();
+
+            return View(student);
+        }
+
 
         public IActionResult Create()
         {
@@ -30,7 +40,7 @@ namespace StudentManagement.Controllers
             {
                 student.Id = students.Any() ? students.Max(s => s.Id) + 1: 1; // Assign a new ID
                 students.Add(student);
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Confirmation), new { id = student.Id });
             }
             return View(student);
         }
--- StudentManagement/Models/Student.cs
+++ StudentManagement/Models/Student.cs
@@ -1,9 +1,19 @@
-﻿namespace StudentManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.Models
 {
     public class Student
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email")]
+
+        public string Email { get; set; } = string.Empty;  
+        
+        [Range(1, 120, ErrorMessage = "Age must be between 1 and 120")]
         public int Age { get; set; }
     }
 }
